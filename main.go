8e9560ae7cc13abe8e3b393af24e6896bd0465ff
TopConnector <constantine@mobiledatabooks.com>
2017-07-18 17:39:53 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	t0 := time.Now()
+	t1 := time.Now()
+
+	t1 = time.Now()
+	t01 := t1.Sub(t0)
+	tStr1 := fmt.Sprintf("Hello World from Go in minimal Docker container(4.28MB) v.2.0, it took %v to run", t01)
+
+	fmt.Fprintln(w, tStr1)
+
+	fmt.Printf("%s", tStr1)
+
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Started, serving at 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
