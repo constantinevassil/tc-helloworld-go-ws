827b5fd2119c37f2abfac7fa399c3c6e85266600
TopConnector <constantine@mobiledatabooks.com>
2017-07-18 17:45:53 -0700

    Update bld.bash

--- bld.bash
+++ bld.bash
@@ -4,13 +4,13 @@
 # license that can be found in the LICENSE file.
 
 # 4,279,416 Jul 14 19:19 helloworld_ws
-echo "update helloworldws"
+echo "update helloworld-go-ws"
 go fmt main.go
 CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' .
-#ldd  helloworldws
+#ldd  helloworld-go-ws
 
-docker build -t helloworldws:v2 .
-docker tag helloworldws:v2 tcmb/helloworldws:v2
-docker push tcmb/helloworldws
+docker build -t helloworld-go-ws:v1 .
+docker tag helloworld-go-ws:v1 topconnector/helloworld-go-ws:v1
+docker push topconnector/helloworld-go-ws
 docker images
 
