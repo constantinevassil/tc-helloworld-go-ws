c5edf2bcd43242f614da85b4713878baa67a881f
TopConnector <constantine@mobiledatabooks.com>
2017-07-20 16:02:02 -0700

    Update bld.bash

--- bld.bash
+++ bld.bash
@@ -4,7 +4,7 @@
 # license that can be found in the LICENSE file.
 
 # 4,279,416 Jul 14 19:19 helloworld_ws
-echo "update helloworld-go-ws"
+echo "update tc-helloworld-go-ws"
 go fmt main.go
 CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' .
 
